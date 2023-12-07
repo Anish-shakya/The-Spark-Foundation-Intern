4bec3a792b6c308a0ececfd2a4d9691416c44f5f
Anish Shakya <shakyaanish69@gmail.com>
2023-12-08 00:18:44 +0545

    EDA progress

--- Task 1/Queries.sql
+++ Task 1/Queries.sql
@@ -16,3 +16,51 @@ CREATE TABLE SuperStore(
 	Profit NUMERIC
 );
 
+select * FROM superstore;
+-- 1) Profitability Analysis
+
+-- 1.1) Profit by Region 
+SELECT region,SUM(profit) AS Total_Profit
+FROM superstore
+GROUP BY region
+ORDER BY Total_Profit DESC;
+
+-- 1.2) Profit By State
+SELECT state,SUM(profit) AS Total_Profit
+FROM superstore
+GROUP BY state
+ORDER BY Total_Profit DESC;
+
+-- 1.3) Profit By city
+SELECT city,SUM(profit) AS Total_Profit
+FROM superstore
+GROUP BY city
+ORDER BY Total_Profit DESC;
+
+-- 2) Category and Sub-Category Analysis
+
+-- 2.1) Most and least  Profitable by categories 
+WITH CTE AS(
+SELECT category,SUM(profit) As total_profit
+FROM superstore
+GROUP BY category
+ORDER BY total_profit DESC
+) 
+SELECT category,total_profit
+FROM CTE 
+WHERE total_profit = (SELECT MAX(total_profit) FROM CTE) 
+	  OR total_profit = (SELECT MIN(total_profit) FROM CTE)
+	
+-- 2.2) Most and least Profitable by categories and sub-categories
+WITH CTE AS(
+	SELECT category,sub_category, SUM(profit) As total_profit
+	FROM superstore
+	GROUP BY category,sub_category
+	ORDER BY total_profit DESC
+)
+SELECT category,sub_category, total_profit
+FROM CTE
+WHERE total_profit = (SELECT MAX(total_profit) FROM CTE)
+	OR total_profit = (SELECT MIN(total_profit) FROM CTE WHERE total_profit > 0)
+ 
+
